f6aa1c7bc7d76ba7be5510d60ac83e58d67fc3f2
dalwrigh <dalewrigh@cisco.com>
2021-11-22 15:25:37 -0500

    added show-run alias

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,4 @@
-alias connect_mariadb_shell="docker exec -it docker_dev-mariadb-1 /bin/bash"
-alias connect_mariadb_db="docker exec -it docker_dev-mariadb-1 mysql -uroot -p"
-alias connect_linux_shell="docker exec -it docker_dev-linux-1 /bin/sh"
+alias connect-mariadb-shell="docker exec -it docker_dev-mariadb-1 /bin/bash"
+alias connect-mariadb-db="docker exec -it docker_dev-mariadb-1 mysql -uroot -p"
+alias connect-linux-shell="docker exec -it docker_dev-linux-1 /bin/sh"
+alias show-run="docker-compose ps"
