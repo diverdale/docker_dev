78eea3408aebdc4f76d24062668991f555715613
dalwrigh <dalewrigh@cisco.com>
2021-11-22 15:19:36 -0500

    adding aliases and correcting network config

---
+++ aliases.sh
@@ -0,0 +1,3 @@
+alias connect_mariadb_shell="docker exec -it docker_dev-mariadb-1 /bin/bash"
+alias connect_mariadb_db="docker exec -it docker_dev-mariadb-1 mysql -uroot -p"
+alias connect_linux_shell="docker exec -it docker_dev-linux-1 /bin/sh"
